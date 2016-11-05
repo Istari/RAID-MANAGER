4571dc08e7bf35859732e432ba7ffc71a3004eda
IstariTheWise <George.A.Cooper@gmail.com>
2016-11-05 15:30:41 -0700

    Removed cached items and added additional .gitignore files

--- cache/templates_c/%%04^04F^04F6A47E%%menu_mainfrm.html.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2016-07-10 22:45:32
-         compiled from menu_mainfrm.html */ ?>
-<td id="leftMenuBorder" width="5px">&nbsp;</td>
-						<td id="contentContainer">
--- cache/templates_c/%%98^98B^98BCFCA6%%error.html.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2016-07-11 05:24:16
-         compiled from error.html */ ?>
-<br>
-<div align="center">
-<div class="errorHeader"><?php echo $this->_tpl_vars['error_data']['error_title']; ?>
-</div>
-<div class="errorBody"><?php echo $this->_tpl_vars['error_data']['error_msg']; ?>
-</div>
-</div>
